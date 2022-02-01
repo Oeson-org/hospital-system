c54e55672bc74d050416e69b62a24276334a77e7
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2022-02-02 02:06:07 +0530

    Added dayjs and updating with new fields in admin slots

--- server/functions/routes/admin-availability.js
+++ server/functions/routes/admin-availability.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const { v4: uuidv4 } = require("uuid");
+const dayjs = require('dayjs')
 const response = require("../services/responses").responseFactory;
 const db = require("../services/db");
 const log = require("../services/logger").log;
@@ -21,18 +22,36 @@ const checkAdmin = (req, res, next) => {
     ));
   }
 };
+function generateSlotMap(start, end, span, gap) {
+  if(!start) return {}
+ let slots = { [start]: null };
+ let lastSlot = dayjs(end).subtract(span + gap, "minute").toJSON();
+ while (lastSlot > start) {
+     let nextStart = dayjs(start)
+         .add(span + gap, "minute")
+         .toJSON();
+     slots[nextStart] = null;
+     start = nextStart;
+ }
+ return slots;
+}
 const createBooking = async (req, res) => {
-  const { from, to, userID } = req.body;
+  const { date, slotspan, starttime, endtime } = req.body;
   log("info", { "Incoming Data": req.body });
   const slot_id = uuidv4();
+  slots = generateSlotMap(starttime, endtime, slotspan, 0);
   const booking = {
-    userID: userID,
-    from: from,
-    to: to,
-    attendee: [],
+    slot_id,
+    date,
+    slotspan,
+    slots,
+    starttime,
+    endtime,
+    appnmnt_id: ""
   };
-  booking["from"] = admin.firestore.Timestamp.fromDate(new Date(booking["from"]));
-  booking["to"] = admin.firestore.Timestamp.fromDate(new Date(booking["to"]));
+  booking["date"] = admin.firestore.Timestamp.fromDate(new Date(booking["date"]));
+  booking["startitme"] = admin.firestore.Timestamp.fromDate(new Date(booking["starttime"]));
+  booking["endtime"] = admin.firestore.Timestamp.fromDate(new Date(booking["endtime"]));
   log("log", { "ID": userID, "Feeding Data": booking });
   const bookingRef = await db.write("slots", slot_id, booking);
   res.status(200).json(response(
